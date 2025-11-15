ce6c1351496f13490ded296a63298b6f27cd1c93
bnc <benjamin.n.canfield@gmail.com>
2025-11-14 20:01:01 -0500

    Updated shadcn aliases to match updated CLI API.

--- apps/docs/src/main.ts
+++ apps/docs/src/main.ts
@@ -1,7 +1,9 @@
-// import './app.css'
-import App from './App.svelte'
+// apps/docs/src/main.ts
 
-const app = new App({
+import App from './App.svelte'
+import { mount } from 'svelte' // 1. Import 'mount'
+
+const app = mount(App, { // 2. Change 'new' to 'mount'
   target: document.getElementById('app')!,
 })
 
